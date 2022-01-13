4c91ff4b32426da1c0f70713440d4a36de02ae74
KBRaghul <raghulbalamurugan15@gmail.com>
2022-01-13 19:27:09 +0530

    first commit

---
+++ action.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$t = RRR
+
+if ($t = "WIPE") {
+  echo "Have a good morning!";
+} elseif ($t = "WEIGH") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
+ 
+</body>
+</html>
